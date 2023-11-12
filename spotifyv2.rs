d469123d1d067c1588135ef89b8511619aacd269
Alex5625 <147171092+Alex5625@users.noreply.github.com>
2023-11-12 19:59:33 -0300

    se agrega pregunta 6 dentro del mismo codigo

--- spotifyv2.rs
+++ spotifyv2.rs
@@ -14,9 +14,9 @@ where P: AsRef<Path>, {
 
 fn loop_rango() -> u32{
     loop{
-        println!("tienes para escoger 5 numeros. \nn3: cantidad de dias que ha pasado desde el lanazmiento.\nn4: cantidad de veces que estuvo en el top 10.\nn6: cantidad de veces que estuvo en el top 1.\nn7: numero de reproducciones en su peak.\nn8: cantidad de reproducciones totales. ");
+        println!("tienes para escoger 5 numeros. \nn3: cantidad de dias que ha pasado desde el lanazmiento.\nn4: cantidad de veces que estuvo en el top 10.\nn6: cantidad de veces que estuvo en el top 1.\nn7: numero de reproducciones en su peak.\nn8: cantidad de reproducciones totales.\nn9: ACCEDE A EJERCICIO 6 ");
         let numero = utiles::texto_numero("columna".to_string());
-        if numero == 3 || numero == 4 || numero == 6 || numero == 7 || numero == 8{
+        if numero == 3 || numero == 4 || numero == 6 || numero == 7 || numero == 8 || numero == 9{
             print_tipo_columna(numero);
             return numero;
         }
@@ -35,6 +35,53 @@ fn print_tipo_columna(numero:u32) -> (){
 
 }
 
+fn ej6() -> () {
+
+    let palabra:String = utiles::ingreso_texto("nombre del artista que desea encontrar".to_string());
+    println!("EL ARTISTA ES {}",palabra);
+    let mut cantidad = 0;
+
+    let mut contador_lineas = 0;
+    if let Ok(lines) = read_lines("./dataSpoti.csv") {
+        // recorrer lineas
+        for line in lines {
+
+            if let Ok(ip) = line {
+                let ip_copy = ip.clone();
+                let split = ip_copy.split(";");
+            //  QUEDA DE LA FORMA ***   ****   **  * *** * 
+                let mut contador_columnas:u32 = 0;
+                // recorrer el split
+                for s in split {
+                    // quiero que me lea desde la segunda linea 
+                    if contador_columnas == 1 && contador_lineas >= 1{
+                        let aaaaa = s.to_lowercase().to_string();
+                        println!("ENTRA A LA CONDICION ARTISTA {}",aaaaa);
+                        // ESTA CONDICION ES EL PROBLEMA Y LA BASE DE LA PREGUNTA 
+                        // si la palabra que yo ingrese anteriormente y la que esta en la posicion 1 de la base de datos
+                        // son iguales, aumenta un contador que se llama cantidad
+                        if palabra == aaaaa{
+                            println!("COINCIDE");
+                            cantidad += 1;
+                        }
+                    }   
+                    contador_columnas += 1;
+                }
+            }
+            contador_lineas += 1;
+        }
+    }
+    println!("El artista {} aparece {} cantidad de veces en la base de datos", palabra,cantidad);
+    
+    if cantidad == 0 {
+        println!("No se encontró en la base de datos, porfavor ingrese bien el nombre del artista\n se reiniciará el programa.\n\n");
+        todo_proceso();
+    } else {
+        println!("El artista {} aparece {} cantidad de veces en la base de datos", palabra,cantidad);
+    }
+}
+
+
 
 
 fn todo_proceso() -> () {
@@ -43,7 +90,10 @@ fn todo_proceso() -> () {
 
     println!("numero de columna a buscar:");
     let columna = loop_rango();
-
+    if columna == 9{
+        ej6();
+        return;
+    }
     let mut contador_lineas = 0;
     if let Ok(lines) = read_lines("./dataSpoti.csv") {
 
